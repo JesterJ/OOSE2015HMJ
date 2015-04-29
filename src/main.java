71374af0192f201f4cc220f83b3ac20889582039
HenrikTheFixer <heibye@gmail.com>
2015-04-29 16:20:31 +0200

    something

---
+++ src/main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+
+public class main {
+
+	public void player(){
+		
+	}
+	public void update(){
+		Scanner s1 = new Scanner(System.in);
+		
+	}
+	public int gravityTiles(int fallNumb){
+		return fallNumb++;
+	}
+
+}
---
+++ src/playingField.java
@@ -0,0 +1,13 @@
+
+public class playingField {
+	int width;
+	int height;
+	boolean isFull = false;
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
