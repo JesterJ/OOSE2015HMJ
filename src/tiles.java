1dbca9bdf154d929743a29e04a7bfd624cb157ac
HenrikTheFixer <heibye@gmail.com>
2015-05-05 12:53:31 +0200

    tiles marc 1

--- src/tiles.java
+++ src/tiles.java
@@ -1,8 +1,51 @@
 
-public class tiles {
-	tiles up;
-	tiles left;
-	
-	
 
+public class tiles {
+	boolean [][] type1 = new boolean[][] {
+		{
+			false,	false,	false,	false,
+			true,	true,	true,	true,
+			false,	false,	false,	false,
+			false,	false,	false,	false,
+		},
+		{
+			false,	false,	true,	false,
+			false,	false,	true,	false,
+			false,	false,	true,	false,
+			false,	false,	true,	false,
+		},
+		{
+			false,	false,	false,	false,
+			false,	false,	false,	false,
+			true,	true,	true,	true,
+			false,	false,	false,	false,
+		},
+		{
+			false,	true,	false,	false,
+			false,	true,	false,	false,
+			false,	true,	false,	false,
+			false,	true,	false,	false,
+		}
+	};
+		
+	private int dimension;
+	private int rows;
+	private int cols;
+	private boolean[][] type;
+	private int spawnCol;
+	private int spawnRow;
+	private tiles curTile;
+	
+	tiles Type1 = TileType(4, 4, 1, type1);
+	
+private tiles TileType(int dimension, int cols, int rows, boolean[][] type) {
+	this.dimension = dimension;
+	this.type = type;
+	this.cols = cols;
+	this.rows = rows;
+	
+	this.spawnCol = 5 - (dimension >> 1);	
+	//this.spawnRow = getTopInset	(0);
+	return curTile;
+}
 }
