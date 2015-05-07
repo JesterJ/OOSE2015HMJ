6853356aace7d561ce77d8af33a7433e46f04eec
JesterJ <jesterj00@gmail.com>
2015-05-07 21:25:47 +0200

    LinkList

--- src/LinkList.java
+++ src/LinkList.java
@@ -1,6 +1,31 @@
 import java.util.LinkedList;
-
+import org.newdawn.slick.geom.Shape;
 
 public class LinkList {
-	LinkedList ll = new LinkedList();
+
+	LinkedList<Shape> ll = new LinkedList<Shape>();
+	
+	public void addList(Shape s){
+		ll.add(s);
+	}	
+	
+	public Shape getList(int i){
+		
+		Shape temp;
+		
+		if(i < 0 || i >= ll.size()){
+			return(null);
+		}
+		
+		temp = ll.get(i);
+		
+		return(temp);
+	}
+	
+	public int listLength(){
+		
+		return ll.size();
+		
+	}
+	
 }
