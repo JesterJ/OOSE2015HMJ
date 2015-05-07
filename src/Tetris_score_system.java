e336d77612179eb47450008fba8ed196478a50ee
Bedyl <be_dyl@hotmail.com>
2015-05-08 01:16:18 +0200

    Idea of score system
    needs cleanup variables and graphical appearance.

--- src/Tetris_score_system.java
+++ src/Tetris_score_system.java
@@ -1,1 +1,27 @@
+int lane1 = 20;
+int lane2 = 50;
+int lane3 = 80;
+int lane4 = 120;
+int score = 0;
+int mScore;
 
+if(elimination == 1){
+  mScore = score + lane1;
+}
+
+if(elimination == 2){
+  mScore = score + lane2;
+}
+
+if(elimination == 3){
+  mScore = score + lane3;
+}
+
+if(elimination == 4){
+  mScore = score + lane4;
+}
+
+score = mScore;
+
+
+}
