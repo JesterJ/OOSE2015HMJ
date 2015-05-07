0f5339e0033ee0d83b23181c696880e9c8bf382b
HenrikTheFixer <heibye@gmail.com>
2015-05-07 16:33:06 +0200

    falling tiles

--- workspace/OOSE2015HMJ/src/tiles.java
+++ workspace/OOSE2015HMJ/src/tiles.java
@@ -7,6 +7,8 @@ import org.newdawn.slick.SlickException;
 
 public class tiles extends BasicGame {
 	public Image Square = null;
+	public Image Type1 = null;
+	int fall = 2;
 	
 	public tiles(String title) {
 		super(title);
@@ -23,13 +25,20 @@ public class tiles extends BasicGame {
 	}
 	
 	public void update(GameContainer arg0, int arg1) throws SlickException {
-			
+	
+			fall = fall + 2;
+			if (fall >= 550)
+				fall = 550;
 		
 	}
 	
 	public void render(GameContainer arg0, Graphics arg1) throws SlickException {
-		Square.draw(350, 0, 50, 50);
-		
-	}
+		Square.draw(350, fall, 50, 50);
 
+	}
+		//The I shaped tile:
+	public void type1 () throws SlickException{
+		Type1 = new Image ("/home/henrik/workspace/OOSE2015/OOSE2015/images/Square.jpg");
+	}
+	
 }
